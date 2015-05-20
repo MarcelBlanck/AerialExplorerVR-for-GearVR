2dd3e68165ca8f6c2f7a4e53a77bad775e84b816
Marcel Blanck <mail@marcel-blanck.de>
2015-05-20 20:39:40 +0200

    Inital Commit - Project Set up for Gear VR build. Splash screen customized

---
+++ Build/Android/src/com/epicgames/ue4/DownloadShim.java
@@ -0,0 +1,9 @@
+package com.epicgames.ue4;
+
+import vr.mighty.pirates.AerialExplorerVR.OBBDownloaderService;import vr.mighty.pirates.AerialExplorerVR.DownloaderActivity;
+
+public class DownloadShim
+{
+	public static OBBDownloaderService DownloaderService;
+	public static DownloaderActivity DownloadActivity;
+	public static Class<DownloaderActivity> GetDownloaderType() { return DownloaderActivity.class; }}
---
+++ Source/AerialExplorerVR/AerialExplorerVRGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "AerialExplorerVRGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AERIALEXPLORERVR_API AAerialExplorerVRGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
