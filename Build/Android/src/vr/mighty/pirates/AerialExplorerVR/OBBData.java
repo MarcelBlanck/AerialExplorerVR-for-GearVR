626b39c618b174ac994d7bae8950daade4a139f1
Marcel Blanck <mail@marcel-blanck.de>
2015-05-24 23:14:04 +0200

    Settings Changes

--- Build/Android/src/vr/mighty/pirates/AerialExplorerVR/OBBData.java
+++ Build/Android/src/vr/mighty/pirates/AerialExplorerVR/OBBData.java
@@ -17,7 +17,7 @@ public static final XAPKFile[] xAPKS = {
 new XAPKFile(
 true, // true signifies a main file
 "1", // the version of the APK that the file was uploaded against
-70063356 // the length of the file in bytes
+70052849 // the length of the file in bytes
 )
 };
 };
