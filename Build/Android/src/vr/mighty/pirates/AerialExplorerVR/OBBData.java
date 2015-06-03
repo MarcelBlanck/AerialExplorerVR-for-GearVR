ec7fe5c4527186b58fcf9098a4ddc58206489809
Marcel Blanck <mail@marcel-blanck.de>
2015-06-04 01:24:01 +0200

    Small adjustments on distance to video screen

--- Build/Android/src/vr/mighty/pirates/AerialExplorerVR/OBBData.java
+++ Build/Android/src/vr/mighty/pirates/AerialExplorerVR/OBBData.java
@@ -17,7 +17,7 @@ public static final XAPKFile[] xAPKS = {
 new XAPKFile(
 true, // true signifies a main file
 "1", // the version of the APK that the file was uploaded against
-69922830 // the length of the file in bytes
+13032099 // the length of the file in bytes
 )
 };
 };
